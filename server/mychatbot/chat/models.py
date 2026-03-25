dd278dc5a263dd9c12e198524de70ac899d6f583
Manidharprapurna <manidhargoud.2001@gmail.com>
2026-03-25 11:40:16 +0530

    restored

---
+++ server/mychatbot/chat/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Department(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class FAQ(models.Model):
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='faqs')
+    question = models.CharField(max_length=255)
+    answer = models.TextField()
+
+    def __str__(self):
+        return f"{self.department.name} - {self.question}"
